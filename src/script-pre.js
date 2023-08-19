ecc14b621f90746fd9c85ec340f0527ac14d1bbe
Frank-Pechar <93831849+Frank-Pechar@users.noreply.github.com>
2023-08-19 14:10:07 -0600

    Form input fix

--- src/script-pre.js
+++ src/script-pre.js
@@ -170,10 +170,10 @@ class App {
 
   _newWorkout(e) {
     // validate input data
-    e.preventDefault();
     const validInputs = (...inputs) =>
       inputs.every((inp) => Number.isFinite(inp));
     const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
+
     // e.preventDefault();
 
     // Get data from form
